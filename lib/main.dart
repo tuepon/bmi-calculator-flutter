cf80fdb1ae33759d5a903deb232ed79563050359
tomonori_ueno <t.uepon@gmail.com>
2023-01-08 10:08:42 +0900

    Create myColor and applied it

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  MaterialColor myColor = MaterialColor(
+    0xFF0A0E21,
+    <int, Color>{
+      50: Color(0xFF0A0E21),
+      100: Color(0xFF0A0E21),
+      200: Color(0xFF0A0E21),
+      300: Color(0xFF0A0E21),
+      400: Color(0xFF0A0E21),
+      500: Color(0xFF0A0E21),
+      600: Color(0xFF0A0E21),
+      700: Color(0xFF0A0E21),
+      800: Color(0xFF0A0E21),
+      900: Color(0xFF0A0E21),
+    },
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        // can't use primaryColor and accentColor, use colorScheme instead.
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: myColor)
             .copyWith(secondary: Colors.purple),
       ),
       home: InputPage(),
